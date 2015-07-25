c6d83a2d65b2630bca116df7c7d3c9ec0c62094e
cullenohara <cullenohara@gmail.com>
2015-07-25 01:33:05 -0500

    Message system

---
+++ Assets/Scripts/NPC.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class NPC : MonoBehaviour {
+
+	public string npcName;
+	public List<string> greeting = new List<string>();
+	public List<string> farewell = new List<string>();
+	public Color textColor;
+
+	// Use this for initialization
+	void Start () {
+		greeting.Add("Hello");
+		greeting.Add("Hi");
+		greeting.Add("Good day");
+
+		farewell.Add("Goodbye!");
+		farewell.Add("See you soon.");
+		farewell.Add("Fuck off!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.tag == "Player")
+		{
+			string tempMessage = greeting[Random.Range(0, greeting.Count)] + " " + other.GetComponent<Player>().GetName;
+			other.GetComponent<Player>().ReceiveMessage(npcName, tempMessage, textColor.ToHexStringRGB());
+		}
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		if(other.tag == "Player")
+		{
+			string tempMessage = farewell[Random.Range(0, farewell.Count)];
+			other.GetComponent<Player>().ReceiveMessage(npcName, tempMessage, textColor.ToHexStringRGB());
+		}
+	}
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,7 +2,9 @@
 using System.Collections;
 using UnityEngine.UI;
 using UnityEngine.Events;
+using System.Collections.Generic;
 
+[System.Serializable]
 public class Player : MonoBehaviour {
 
 	private Rigidbody playerRB;
@@ -10,10 +12,22 @@ public class Player : MonoBehaviour {
 	public InputField input;
 	public Text textArea;
 
+	private List<string> messages = new List<string>();
+	public int maxMessages = 30;
+
+	private string playerName = "Cullen";
+	public Color textColor;
+
+	public string GetName
+	{
+		get { return playerName; }
+	}
+
 	// Use this for initialization
 	void Start () {
 		playerRB = GetComponent<Rigidbody> ();
 		input.onEndEdit.AddListener (SubmitMessage);
+		input.characterLimit = 144;
 	}
 	
 	// Update is called once per frame
@@ -41,8 +55,46 @@ public class Player : MonoBehaviour {
 
 	private void SubmitMessage(string arg0)
 	{
-		Debug.Log (arg0);
-		textArea.text += arg0 + "\n";
-		input.text = "";
+		if(arg0 != "")
+		{
+			string tempString = "\n" + "<color=#" + textColor.ToHexStringRGB() + "><b>" + playerName + ":</b> " + arg0 + "</color>";
+			if(maxMessages > messages.Count)
+			{
+				messages.Add(tempString);
+			}
+			else
+			{
+				messages.RemoveAt(0);
+				messages.Add(tempString);
+			}
+
+			input.text = "";
+			LoadMessages();
+		}
+	}
+
+	public void ReceiveMessage(string name, string message, string color)
+	{
+		string tempString = "\n" + "<color=#" + color + "><b>" + name + ":</b> " + message + "</color>";
+		if(maxMessages > messages.Count)
+		{
+			messages.Add(tempString);
+		}
+		else
+		{
+			messages.RemoveAt(0);
+			messages.Add(tempString);
+		}
+
+		LoadMessages();
+	}
+
+	private void LoadMessages()
+	{
+		textArea.text = "";
+		foreach(string msg in messages)
+		{
+			textArea.text += msg;
+		}
 	}
 }
